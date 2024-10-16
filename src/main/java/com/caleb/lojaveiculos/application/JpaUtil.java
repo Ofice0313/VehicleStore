a975a1612a788f837dd09a594db75ba7daabc25f
Marcelo Caleb Júlio Ofice <marcelocalebjulioo@gmail.com>
2024-10-16 10:06:57 +0200

    Creating entityManager for entities in the class JpaUtil and code

--- src/main/java/com/caleb/lojaveiculos/application/JpaUtil.java
+++ src/main/java/com/caleb/lojaveiculos/application/JpaUtil.java
@@ -1,5 +1,23 @@
 package com.caleb.lojaveiculos.application;
 
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Persistence;
+
 public class JpaUtil {
+	
+	private static EntityManagerFactory factory;
+	
+	static {
+		factory = Persistence.createEntityManagerFactory("veiculos-PU");
+	}
+	
+	public static EntityManager getEntityManager() {
+		return factory.createEntityManager();
+	}
+	
+	public static void close() {
+		factory.close();
+	}
 
 }
