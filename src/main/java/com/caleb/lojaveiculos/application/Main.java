42e1e68179c9dce75c2a1306a1665da3d4467f9a
Marcelo Caleb Júlio Ofice <marcelocalebjulioo@gmail.com>
2024-10-16 16:21:36 +0200

    updating value

--- src/main/java/com/caleb/lojaveiculos/application/Main.java
+++ src/main/java/com/caleb/lojaveiculos/application/Main.java
@@ -1,10 +1,12 @@
 package com.caleb.lojaveiculos.application;
 
+import java.math.BigDecimal;
 import java.util.List;
 
 import com.caleb.lojaveiculos.dominio.Veiculos;
 
 import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityTransaction;
 import jakarta.persistence.Query;
 
 public class Main {
@@ -12,19 +14,17 @@ public class Main {
 	public static void main(String[] args) {
 		
 		EntityManager manager = JpaUtil.getEntityManager();
+		EntityTransaction tx = manager.getTransaction();
+		tx.begin();
 		
-		//Listing objects from Veiculos using JPQL
-		Query query = manager.createQuery("select v from Veiculos v");
-		List<Veiculos> veiculos = query.getResultList();
+		//updating objects
+		Veiculos vehicles = manager.find(Veiculos.class, 1L);
 		
-		for(Veiculos vehicles: veiculos) {
-			System.out.println(vehicles.getCodigo() + " -"
-					+ vehicles.getFabricante() + " "
-					+ vehicles.getModelo() + ", ano "
-					+ vehicles.getAnoFabricacao() + " por "
-					+ "R$" + vehicles.getValor());
-		}
+		System.out.println("current value: " + vehicles.getValor());
+		vehicles.setValor(vehicles.getValor().add(new BigDecimal(500)));
+		System.out.println("New value: " + vehicles.getValor());
 		
+		tx.commit();
 		manager.close();
 		JpaUtil.close();
 
